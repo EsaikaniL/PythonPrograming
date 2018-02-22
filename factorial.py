4d0ceb3a3e4722dfa756800c72ca513e2cf4dd44
Nagappansabari <nagappansabari@gmail.com>
2018-02-22 23:05:38 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,8 @@
 x=input("enter the number:")
-z=1
-for y in range(1,x):
-    z=z*y
-print 'factorial of the given number is ' + str(z) 
+if x>=0:
+    z=1
+    for y in range(1,x):
+        z=z*y
+    print 'factorial of the given number is ' + str(z) 
+else:
+        print 'enter the valid number'
