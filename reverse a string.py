2b12018696a7ec570b52fd01328e9d2092bd18ae
Nagappansabari <nagappansabari@gmail.com>
2018-02-23 15:49:35 +0530

    Update reverse a string.py

--- reverse a string.py
+++ reverse a string.py
@@ -2,8 +2,6 @@ x=raw_input("enter the string:")
 n=len(x)
 e=''
 for  y in range(-1,-(n+1),-1):
-   
-    if x[y]!='a' and x[y]!='e' and x[y]!='i' and x[y]!='o' and x[y]!='u':
         z=x[y]
         e=e+z
 print(e)
