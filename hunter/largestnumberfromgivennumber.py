75c88ddcd12f893f2123fa1d0e71df09412824ce
Nagappansabari <nagappansabari@gmail.com>
2018-03-21 15:40:20 +0530

    Update largestnumberfromgivennumber.py

--- hunter/largestnumberfromgivennumber.py
+++ hunter/largestnumberfromgivennumber.py
@@ -3,6 +3,11 @@ l=[]
 sum=''
 for x in range(0,num):
     l.append(input())
+    if l[x]>0:
+        pass
+    else:
+        print "enter valid number"
+        exit()
 l.sort()
 for x in range(-1,-(num+1),-1):
     sum=sum+str(l[x])
