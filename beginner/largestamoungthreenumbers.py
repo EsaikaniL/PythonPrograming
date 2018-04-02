6a55e8ccbe9c887aec0a0f6c2f101a3e201144b8
Nagappansabari <nagappansabari@gmail.com>
2018-04-02 17:07:27 +0530

    Create largestamoungthreenumbers.py

---
+++ beginner/largestamoungthreenumbers.py
@@ -0,0 +1,13 @@
+a=input("enter the first number:")
+b=input("enter the secound number:")
+c=input("enter the third number:")
+if a>b:
+    if a>c:
+        print a,"is greater"
+    else:
+        print c,"is greater"
+else:
+    if b>c:
+        print b,"is greater"
+    else:
+        print c,"is greater"
